0a5eb1ab2e9444ffc68a35a2cfe68989521005f7
shiipou <shiishii@nocturlab.fr>
2022-03-14 16:40:23 +0100

    fix: module.export needed
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import core from '@actions/core'
-import github from '@actions/github'
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-export async function run() {
+module.exports = async function run() {
   const client = github.getOctokit(process.env.GITHUB_TOKEN, {
     baseUrl: process.env.INPUT_GITHUBBASEURL,
   })
